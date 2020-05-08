4e2ad017e928a7ee22fcfe063f22d57201577283
Teddy Wing <git@teddywing.com>
2020-05-08 18:13:46 +0200

    styles: Hide skip buttons

--- src/styles.ts
+++ src/styles.ts
@@ -5,18 +5,32 @@ export function styles () {
 
 	const stylesheet = style.sheet as CSSStyleSheet;
 
+	// "Back to Browse" button that appears when credits are minimised.
 	stylesheet.insertRule(
 		'.OriginalsPostPlay-BackgroundTrailer .BackToBrowse { visibility: hidden; }',
 		stylesheet.cssRules.length
 	);
 
+	// Age rating.
 	stylesheet.insertRule(
 		'.player-view-childrens { visibility: hidden; }',
 		stylesheet.cssRules.length
 	);
 
+	// "Watch Credits" button.
 	stylesheet.insertRule(
 		'[data-uia="watch-credits-seamless-button"] { visibility: hidden; }',
+		stylesheet.cssRules.length
+	);
+
+	// Skip buttons.
+	stylesheet.insertRule(`
+		a[aria-label="Skip Intro"],
+		a[aria-label="Skip Recap"],
+		a[aria-label="Next Episode"],
+		[data-uia="next-episode-seamless-button"] {
+			visibility: hidden;
+		}`,
 		stylesheet.cssRules.length
 	);
 }
