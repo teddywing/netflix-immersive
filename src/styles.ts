8a75b2b5c2963c464596abfd45bd64183980f4e3
Teddy Wing <git@teddywing.com>
2020-05-08 18:29:15 +0200

    styles: Combine all our rules into one
    Since we set `visibility: hidden` for all elements, combine them all
    into a single CSS rule.

--- src/styles.ts
+++ src/styles.ts
@@ -5,26 +5,17 @@ export function styles () {
 
 	const stylesheet = style.sheet as CSSStyleSheet;
 
-	// "Back to Browse" button that appears when credits are minimised.
-	stylesheet.insertRule(
-		'.OriginalsPostPlay-BackgroundTrailer .BackToBrowse { visibility: hidden; }',
-		stylesheet.cssRules.length
-	);
-
-	// Age rating.
-	stylesheet.insertRule(
-		'.player-view-childrens { visibility: hidden; }',
-		stylesheet.cssRules.length
-	);
-
-	// "Watch Credits" button.
-	stylesheet.insertRule(
-		'[data-uia="watch-credits-seamless-button"] { visibility: hidden; }',
-		stylesheet.cssRules.length
-	);
-
-	// Skip buttons.
 	stylesheet.insertRule(`
+		/* "Back to Browse" button that appears when credits are minimised. */
+		.OriginalsPostPlay-BackgroundTrailer .BackToBrowse,
+
+		/* Age rating. */
+		.player-view-childrens,
+
+		/* "Watch Credits" button. */
+		[data-uia="watch-credits-seamless-button"],
+
+		/* Skip buttons. */
 		a[aria-label="Skip Intro"],
 		a[aria-label="Skip Recap"],
 		a[aria-label="Next Episode"],
