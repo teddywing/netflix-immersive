998a674d6ddedf1e0f9f5f51843e0b4d72198975
Teddy Wing <git@teddywing.com>
2020-05-08 22:36:40 +0200

    Add license (GNU GPLv3+)

--- Makefile
+++ Makefile
@@ -1,3 +1,20 @@
+# Copyright (c) 2020  Teddy Wing
+#
+# This file is part of Immersive.
+#
+# Immersive is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Immersive is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Immersive. If not, see <https://www.gnu.org/licenses/>.
+
 BROWSERIFY := ./node_modules/.bin/browserify
 TSC := ./node_modules/.bin/tsc
 
@@ -6,6 +23,9 @@ all: netflix-immersive.user.js
 
 build/%.js: src/%.ts
 	$(TSC)
+
+	@# Remove license headers
+	perl -ni -e 'print unless ($$record_count = /^\/\/ Copyright/ .. /^(?!\/\/)/ and $$record_count !~ /E0$$/)' $@
 
 netflix-immersive.user.js: build/index.js build/*.js src/userscript-header.txt
 	$(BROWSERIFY) $< -o $@
--- netflix-immersive.user.js
+++ netflix-immersive.user.js
@@ -7,6 +7,21 @@
 // @match https://www.netflix.com/watch/*
 // ==/UserScript==
 
+// Copyright (c) 2020  Teddy Wing
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
